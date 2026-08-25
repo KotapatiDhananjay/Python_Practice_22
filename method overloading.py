26eb902fcba3f5674955ec0f2ab1712fdc155fd4
Kotapati Dhananjay <your-email@example.com>
2026-08-25 14:37:32 +0530

    Initial commit

---
+++ method overloading.py
@@ -0,0 +1,9 @@
+class A:
+    def add(self, num1, num2):
+        return num1 + num2
+
+    def add(self, num1, num2, num3):
+        return num1 + num2 + num3
+
+obj = A()
+print(obj.add(10, 20, 30))
